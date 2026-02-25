b751a718c9f24e4a4fd6ba4d63e76a096bab5817
anchasatapathy45 <anchalsatapathy45@gmail.com>
2026-02-26 01:26:32 +0530

    25 Feb Practice

---
+++ src/ex06_25thFeb_Learn_Java/PrintNumber.java
@@ -0,0 +1,10 @@
+package ex06_25thFeb_Learn_Java;
+
+public class PrintNumber {
+    public static void main(String args[]){
+        for(int i=0; i<=10; i++){
+            System.out.println("Number is "+i);
+        }
+        System.out.println("All numbers are printed.");
+    }
+}
