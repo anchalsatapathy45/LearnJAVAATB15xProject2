d195a01b71ef1feaea4482aeacef30c03e5cf84f
anchasatapathy45 <anchalsatapathy45@gmail.com>
2026-02-27 02:24:46 +0530

    25 Feb Practice

--- src/ex06_25thFeb_Learn_Java/EvenNumbers.java
+++ src/ex06_25thFeb_Learn_Java/EvenNumbers.java
@@ -1,7 +1,7 @@
 package ex06_25thFeb_Learn_Java;
 
 public class EvenNumbers{
-    public static void main(String args[]){
+    public static void main(String[] args){/*modern approach of array declaration*/
         for(int i=1; i<=20; i++){
             if(i%2==0) {
                 System.out.println(i);
---
+++ src/ex06_25thFeb_Learn_Java/LearnMore.java
@@ -0,0 +1,13 @@
+package ex06_25thFeb_Learn_Java;
+
+public class LearnMore {
+    public static void main(String args[]){
+        for(int i=0; i<=4; i++){
+            if(i<=2)
+            System.out.println("Growing");
+
+        else
+            System.out.println("Wondering");
+        }
+    }
+}
